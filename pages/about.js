7b05bb921289038d260f3867bc30f8d41e19db16
mganeko <masashi.g@gmail.com>
2021-11-27 22:16:31 +0900

    about page

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,12 @@
+
+import Layout from "../components/Layout"
+
 const About = () => {
-  return <div>ブログを書いています。</div>
+  return (
+    <Layout title="">
+      <div>oralabo のブログです。</div>
+      <div>React.jsを使ったブログのサンプル <a href="https://github.com/gh640/nextjs-blog-example-ja">gh640/nextjs-blog-example-ja</a>（MITライセンス）を利用しています</div>
+    </Layout>)
 }
 
 export default About
