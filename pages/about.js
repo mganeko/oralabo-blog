e80e23989cc8d43fa922608ec530ac715f53e482
mganeko <masashi.g@gmail.com>
2021-11-28 23:01:32 +0900

    add npm package list

--- pages/about.js
+++ pages/about.js
@@ -6,6 +6,18 @@ const About = () => {
     <Layout title="">
       <div>oralabo のブログです。</div>
       <div>React.jsを使ったブログのサンプル <a href="https://github.com/gh640/nextjs-blog-example-ja">gh640/nextjs-blog-example-ja</a>（MITライセンス）を利用しています</div>
+      <div>
+       また、以下の NPM パッケージを使用しています。
+       <ul>
+         <li>next</li>
+         <li>react</li>
+         <li>`react-dom`</li>
+         <li>`remark`</li>
+         <li>`remark-html`</li>
+         <li>`remark-prism`</li>
+         <li>gray-matter`</li>
+       </ul>
+      </div>
     </Layout>)
 }
 
