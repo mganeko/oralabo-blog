1a14944fe14f62ddfb4216845e007494c2269f32
mganeko <masashi.g@gmail.com>
2021-12-03 22:08:36 +0900

    fix about

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <Layout title="">
       <div>oralabo のブログです。</div>
-      <div>React.jsを使ったブログのサンプル <a href="https://github.com/gh640/nextjs-blog-example-ja">gh640/nextjs-blog-example-ja</a>（MITライセンス）を利用しています</div>
+      <div>React.jsベースのNext.jsを使ったブログのサンプル <a href="https://github.com/gh640/nextjs-blog-example-ja">gh640/nextjs-blog-example-ja</a>（MITライセンス）を利用しています</div>
       <div>
        また、以下の NPM パッケージを使用しています。
        <ul>
